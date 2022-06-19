a144b74d3801d1dac44232409867e809f392db37
Lilit33 <l.vardanyan48@mail.ru>
2022-06-19 15:13:26 +0400

    second

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 
+//<<<<<<< HEAD
 int print3();
+//=======
+
+int print2();
+//>>>>>>> test_1
 
 int main ()
 {
     std::cout << "Hello World" << std::endl;
+//<<<<<<< HEAD
     print3();
+//=======
+ 
+    print2();
+//>>>>>>> test_1
     return 0;
 }
 
@@ -13,5 +23,11 @@ int print3 ()
 {
     std::cout << "Second Branch" << std::endl;
 
+    return 0;
+}
+
+int print2 ()
+{
+    std::cout << "first branch" << std::endl;
     return 0;
 }
