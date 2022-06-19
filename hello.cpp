ef204c56227690784c2f025c039a06bc27546414
Lilit33 <l.vardanyan48@mail.ru>
2022-06-19 15:00:30 +0400

    second branch

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+int print3();
+
 int main ()
 {
     std::cout << "Hello World" << std::endl;
+    print3();
+    return 0;
+}
+
+int print3 ()
+{
+    std::cout << "Second Branch" << std::endl;
+
     return 0;
 }
