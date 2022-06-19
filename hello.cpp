0e10e776777b8fe7bf7025adc5a52205ff441ab6
Lilit33 <l.vardanyan48@mail.ru>
2022-06-19 13:50:47 +0400

    Second Commit

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+int print();
+
 int main ()
 {
     std::cout << "Hello World" << std::endl;
+    print();
+    return 0;
+}
+
+int print ()
+{
+    std::cout << "Second Commit" << std::endl;
+
     return 0;
 }
