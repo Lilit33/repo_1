de03def4ea40fa2d2ba0d35390a52897f6728530
Lilit33 <l.vardanyan48@mail.ru>
2022-06-19 14:39:16 +0400

    Revert "Second Commit"
    This reverts commit 0e10e776777b8fe7bf7025adc5a52205ff441ab6.

--- hello.cpp
+++ hello.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
 
-int print();
-
 int main ()
 {
     std::cout << "Hello World" << std::endl;
-    print();
-    return 0;
-}
-
-int print ()
-{
-    std::cout << "Second Commit" << std::endl;
-
     return 0;
 }
